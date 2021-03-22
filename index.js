3d697c768c9d3419d2691e38b7665303c006c1ac
Antoni Kepinski <xxczaki@pm.me>
2021-03-22 01:05:20 +0100

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+import Discord from 'discord.js';
+import levenshtein from 'fast-levenshtein';
+
+import {countries, dates} from './utils.js';
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+  if (msg.content === '!wc') {
+    msg.reply(`you have committed over ${Math.floor(Math.random() * (900 - 20) + 20)} war crimes in ${countries[Math.floor(Math.random() * countries.length)]}.`);
+  } else if (msg.content === '!nys') {
+      msg.reply(`jesteś Nysem w ${Math.floor(Math.random() * (120 - 2) + 2)}%`);
+  } else if (msg.content === '!chungus') {
+      msg.reply(`jesteś Chungusem w ${Math.floor(Math.random() * (120 - 2) + 2)}%`);
+  } else if (levenshtein.get(msg.content, 'among us') <= 3) {
+      msg.reply(`cringe/10`);
+  } else if (levenshtein.get(msg.content, '!doin') <= 3) {
+      const random = await msg.guild.members.cache.random().id;
+
+      msg.reply(`I can confirm that <@!${random}> did your mum ${dates[Math.floor(Math.random() * dates.length)]}.`);
+  }
+});
+
+client.login('NzY3MTEzNDQxNzE4NDM1ODUw.X4tMEA.3x43qkbbpjRZtu7pSE73QjUOu2w');
---
+++ utils.js
@@ -0,0 +1,27 @@
+export const countries = [
+    'Argentina',
+    'Chile',
+    'South Sudan',
+    'Brazil',
+    'Uganda',
+    'Swaziland',
+    'Japan',
+    'Kyrgyzstan',
+    'Pakistan',
+    'Mongolia',
+    'South Africa'
+];
+
+export const dates = [
+    'last Thursday',
+    'last week',
+    'last Saturday',
+    '3 days ago',
+    'a week ago',
+    '2 days ago',
+    'yesterday',
+    '1 year ago',
+    'last Monday',
+    'last Wednesday',
+    '4 days ago'
+];
