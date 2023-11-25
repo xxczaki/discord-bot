8506076eecd1cc17ecfbbc106ca68b99861381f1
Antoni Kepinski <antoni@kepinski.ch>
2023-11-25 13:07:48 +0100

    upd

--- index.js
+++ index.js
@@ -1,70 +1,89 @@
-import {Client, Intents, MessageEmbed} from 'discord.js';
-import levenshtein from 'fast-levenshtein';
-import {randomInt} from 'node:crypto';
+const { Client, GatewayIntentBits } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+const { Player, useQueue } = require('discord-player');
+const play = require('./play');
 
-import {countries, dates, criminals} from './utils.js';
+const TOKEN = 'NzY3MTEzNDQxNzE4NDM1ODUw.G-v3ac.wIoY9UqD25_6ztWEGCT0pyrL7vYUyCxWqudHW0';
+const CLIENT_ID = '767113441718435850';
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const COURTROOM_A_ID = '824396697558843436';
+
+const commands = [
+  {
+    name: 'play',
+    description: 'Plays some music',
+    options: [
+      {
+        name: 'query',
+        description: 'Searches for a song',
+        type: 3,
+        required: true
+      }
+    ]
+  },
+  {
+    name: 'pause',
+    description: 'Pauses the queue',
+  },
+  {
+    name: 'resume',
+    description: 'Resumes the queue',
+  },
+  {
+    name: 'skip',
+    description: 'Skips the current track',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const player = new Player(client);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+  
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+  
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+
+  await player.extractors.loadDefault();
+})();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', async msg => {
-  try {
-    if (msg.content === '!wc') {
-      msg.reply(`you have committed ${randomInt(900)} war crimes in ${countries[randomInt(countries.length)]}.`);
-    } else if (msg.content === '!nys') {
-        msg.reply(`jesteś Nysem w ${randomInt(120)}%`);
-    } else if (msg.content === '!chungus') {
-        msg.reply(`jesteś Chungusem w ${randomInt(120)}%`);
-    } else if (levenshtein.get(msg.content, 'among us') <= 3) {
-        msg.reply(`cringe/10`);
-    } else if (msg.content === '!doin') {
-        const random = msg.guild.members.cache.random().user.id;
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-        msg.reply(`I can confirm that <@!${random}> did your mum ${dates[randomInt(dates.length)]}.`);
-    } else if (msg.content === '!criminal') {
-        const random = criminals[randomInt(criminals.length)];
+  const queue = useQueue(interaction.guild.id);
 
-        const embed = new MessageEmbed();
-
-        embed.color = '#0099ff';
-        embed.title = `Jesteś ${random.name}!`;
-        embed.url = random.url;
-        embed.description = `Zostałeś skazany na ${random.sentence === Infinity ? 'dożywocie' : `${random.sentence} lat więzienia`} przez Międzynarodowy Trybunał Karny dla byłej Jugosławii w Hadze.`;
-        embed.thumbnail = random.image ? {url: random.image} : undefined;
-        embed.footer = {text: 'Dane: Wikipedia'};
-
-        msg.channel.send({embeds: [embed]});
-    } else if (msg.content === '!help') {
-        const embed = new MessageEmbed();
-
-        embed.color = '#0099ff';
-        embed.title = `Dostępne komendy:`;
-        embed.description =
-        `
-        !wc
-        !nys
-        !chungus
-        !doin
-        !criminal
-        !help
-        `;
-
-        msg.channel.send({embeds: [embed]});
-    }
-  } catch (error) {
-      const embed = new MessageEmbed();
-
-      embed.color = '#ff0000';
-      embed.title = `Silenced error`;
-      embed.description = `Ping <@!349213466743013376>, dodatkowe informacje:\n\n\`${error.message ?? 'brak'}\``;
-      embed.footer = {text: 'Bot kontynuuje normalną pracę.'};
-
-      msg.channel.send({embeds: [embed]});
+  if (interaction.commandName === 'play') {
+    await play(interaction);
   }
 
+  if (interaction.commandName === 'pause') {
+    queue.node.setPaused(true);
+
+    await interaction.followUp('Track paused');
+  }
+
+  if (interaction.commandName === 'resume') {
+    queue.node.setPaused(false);
+
+    await interaction.followUp('Track resumed');
+  }
+
+  if (interaction.commandName === 'skip') {
+    queue.node.skip();
+
+    await interaction.followUp('Track skipped');
+  }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(TOKEN);
---
+++ play.js
@@ -0,0 +1,26 @@
+const { useMainPlayer } = require('discord-player');
+
+module.exports = async function play(interaction) {
+    const player = useMainPlayer();
+    const channel = interaction.member.voice.channel;
+    if (!channel) return interaction.reply('You are not connected to a voice channel!'); // make sure we have a voice channel
+    const query = interaction.options.getString('query', true); // we need input/query to play
+
+    // let's defer the interaction as things can take time to process
+    await interaction.deferReply();
+
+    try {
+        const { track } = await player.play(channel, query, {
+            nodeOptions: {
+                // nodeOptions are the options for guild node (aka your queue in simple word)
+                metadata: interaction, // we can access this metadata object using queue.metadata later on
+                selfDeaf: false,
+            }
+        });
+
+        return interaction.followUp(`**${track.title}** enqueued!`);
+    } catch (e) {
+        // let's return error if something failed
+        return interaction.followUp(`Something went wrong: ${e}`);
+    }
+}
