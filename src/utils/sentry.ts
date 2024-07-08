4c7e4b2df576f1a66030f708f321538dab8f9124
Antoni Kępiński <antoni@kepinski.ch>
2024-07-08 11:58:06 +0200

    remove sentry profiling

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,13 +1,6 @@
 import * as Sentry from '@sentry/node';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import getEnvironmentVariable from './getEnvironmentVariable';
 
 Sentry.init({
 	dsn: getEnvironmentVariable('SENTRY_DSN'),
-	integrations: [nodeProfilingIntegration()],
-	// Performance Monitoring
-	tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-	// Set sampling rate for profiling - this is relative to tracesSampleRate
-	profilesSampleRate: 1.0,
 });
