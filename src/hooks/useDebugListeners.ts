8e60a2255061689b1c0e05877faea854513c1884
Antoni Kępiński <antoni@kepinski.ch>
2024-10-16 22:00:02 +0200

    updates

--- src/hooks/useDebugListeners.ts
+++ src/hooks/useDebugListeners.ts
@@ -1,13 +1,19 @@
+import { createServer } from 'node:net';
 import { useMainPlayer } from 'discord-player';
 import type { Client } from 'discord.js';
 import logger from '../utils/logger';
 
 export default function useDebugListeners(client: Client<boolean>) {
+	const server = createServer();
+
+	server.listen(8000);
+
 	process.on('unhandledRejection', (reason) => {
 		logger.error(reason, 'Unhandled promise rejection');
 	});
 	process.on('uncaughtException', (error) => {
 		logger.error(error, 'Uncaught exception');
+		server.close();
 	});
 
 	client.on('error', (error) => logger.error(error, 'Client error'));
--- src/utils/getReleaseDetails.ts
+++ src/utils/getReleaseDetails.ts
@@ -1,5 +1,6 @@
 export default function getReleaseDetails() {
-	const commitHash = process.env.GIT_COMMIT ?? process.env.RAILWAY_GIT_COMMIT_SHA;
+	const commitHash =
+		process.env.GIT_COMMIT ?? process.env.RAILWAY_GIT_COMMIT_SHA;
 	const wasDeploymentManual = !commitHash;
 
 	const commitLink = `[\`${commitHash}\`](<https://github.com/xxczaki/discord-bot/commit/${commitHash}>)`;
