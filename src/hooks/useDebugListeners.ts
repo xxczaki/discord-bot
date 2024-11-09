3608409285c7f117e1ae7a921e6cd80537d43c94
Antoni Kępiński <22645979+xxczaki@users.noreply.github.com>
2024-11-09 22:22:40 +0100

    infra

--- src/hooks/useDebugListeners.ts
+++ src/hooks/useDebugListeners.ts
@@ -3,12 +3,18 @@ import { useMainPlayer } from 'discord-player';
 import type { Client } from 'discord.js';
 import logger from '../utils/logger';
 
+const server = createServer();
+
+server.listen(8000);
+
 export default function useDebugListeners(client: Client<boolean>) {
 	process.on('unhandledRejection', (reason) => {
 		logger.error(reason, 'Unhandled promise rejection');
+		server.close();
 	});
 	process.on('uncaughtException', (error) => {
 		logger.error(error, 'Uncaught exception');
+		server.close();
 	});
 
 	client.on('error', (error) => logger.error(error, 'Client error'));
@@ -23,8 +29,4 @@ export default function useDebugListeners(client: Client<boolean>) {
 
 	player.on('debug', (message) => logger.debug({}, message));
 	player.events.on('debug', (_, message) => logger.debug({}, message));
-
-	const server = createServer();
-
-	server.listen(8000);
 }
