2b8597ade025c2b96799e584ecd7fcb571d3e53f
Antoni Kępiński <22645979+xxczaki@users.noreply.github.com>
2024-12-24 12:15:44 +0100

    fix

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -3,6 +3,6 @@ import getEnvironmentVariable from './getEnvironmentVariable';
 
 const redis = new Redis(getEnvironmentVariable('REDIS_URL'));
 
-// redis.options.family = 6;
+redis.options.family = 6;
 
 export default redis;
