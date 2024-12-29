f4cde3a440eca6471f2bab5b1c0e1ca8a679ee9e
Antoni Kępiński <22645979+xxczaki@users.noreply.github.com>
2024-12-29 12:03:07 +0100

    update

--- src/utils/getReleaseDetails.ts
+++ src/utils/getReleaseDetails.ts
@@ -1,6 +1,5 @@
 export default function getReleaseDetails() {
-	const commitHash =
-		process.env.GIT_COMMIT ?? process.env.RAILWAY_GIT_COMMIT_SHA;
+	const commitHash = process.env.GIT_COMMIT_SHA;
 	const wasDeploymentManual = !commitHash;
 
 	const commitLink = `[\`${commitHash}\`](<https://github.com/xxczaki/discord-bot/commit/${commitHash}>)`;
