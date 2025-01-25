53481db643dc62b65b35180538567a8e7e2ab48a
Antoni Kępiński <22645979+xxczaki@users.noreply.github.com>
2025-01-25 21:33:52 +0100

    fix

--- src/utils/createTrackEmbed.ts
+++ src/utils/createTrackEmbed.ts
@@ -8,7 +8,5 @@ export default function createTrackEmbed(track: Track, description: string) {
 		.setURL(track.url)
 		.setAuthor({ name: track.author })
 		.setThumbnail(URL.canParse(track.thumbnail) ? track.thumbnail : null)
-		.addFields([
-			{ name: 'Duration', value: track.duration, inline: true },
-		]);
+		.addFields([{ name: 'Duration', value: track.duration, inline: true }]);
 }
